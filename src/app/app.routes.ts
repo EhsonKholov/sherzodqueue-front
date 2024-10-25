df25afba007762cf25e309e6b61ea667c1a23426
Ehsonjon Kholov <ehson.kholov@gmail.com>
2024-10-25 14:35:32 +0500

    Routing is configured, the login page layout is ready.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,18 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
+import {AdminComponent} from './pages/admin/admin/admin.component';
+import {ClientsComponent} from './pages/admin/clients/clients.component';
+import {LoginComponent} from './pages/admin/login/login.component';
+import {NotFoundComponent} from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full'},
-  {path: 'admin', }
+  {
+    path: 'admin', component: AdminComponent, children: [
+      {path: '', redirectTo: '/clients', pathMatch: 'full'},
+      {path: 'clients', component: ClientsComponent},
+      {path: 'login', component: LoginComponent},
+    ]
+  },
+  {path: '**', component: NotFoundComponent},
 ];
--- src/app/pages/admin/login/login.component.ts
+++ src/app/pages/admin/login/login.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, signal, WritableSignal} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  togglePassword: WritableSignal<boolean> = signal(false)
+  loginFormGroup: FormGroup = new FormGroup({
+    login: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(3)]),
+    password: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(3)]),
+  })
+
+  constructor(private authService: AuthService) {}
+
+  login() {
+  }
 }
