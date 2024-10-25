ff00c1c2bf11ded1817b6cb0393482a757c070a8
Ehsonjon Kholov <ehson.kholov@gmail.com>
2024-10-25 16:19:25 +0500

    Lazy loading for admin component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
-import {AdminComponent} from './pages/admin/admin/admin.component';
 import {ClientsComponent} from './pages/admin/clients/clients.component';
 import {LoginComponent} from './pages/admin/login/login.component';
 import {NotFoundComponent} from './pages/not-found/not-found.component';
@@ -8,7 +7,7 @@ import {NotFoundComponent} from './pages/not-found/not-found.component';
 export const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full'},
   {
-    path: 'admin', component: AdminComponent, children: [
+    path: 'admin', loadComponent: () => import('./pages/admin/admin/admin.component').then(m => m.AdminComponent), children: [
       {path: '', redirectTo: '/clients', pathMatch: 'full'},
       {path: 'clients', component: ClientsComponent},
       {path: 'login', component: LoginComponent},
